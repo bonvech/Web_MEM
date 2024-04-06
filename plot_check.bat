79d7f11c72ebe5f8ec826e4a5cb640994e1c950f
bonvech <bonvech@yandex.ru>
2024-04-06 14:46:55 +0300

    bat files

--- plot_check.bat
+++ plot_check.bat
@@ -2,9 +2,5 @@ echo
 
 D:
 cd D:\AK\Web_MEM\
-
-python main.py 
-rem  > logtest.txt
 python webmem_plot_figures.py
-
 pause
--- start.bat
+++ start.bat
@@ -1,7 +1,9 @@
 echo
 
+D:
 cd D:\AK\Web_MEM\
-start /min python main.py
+
+start /min python main.py > logg.txt
 start /min python webmem_plot_figures.py
 rem pause
 
