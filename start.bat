772272d18efe02c1a08b770a53ca3b829b251f2a
bonvech <bonvech@yandex.ru>
2024-04-06 14:05:53 +0300

    bat files

---
+++ start.bat
@@ -0,0 +1,7 @@
+echo
+
+cd D:\AK\Web_MEM\
+start /min python main.py
+start /min python webmem_plot_figures.py
+rem pause
+
---
+++ start_check.bat
@@ -0,0 +1,8 @@
+echo
+
+cd D:\AK\Web_MEM\
+
+python main.py
+python webmem_plot_figures.py
+
+pause
